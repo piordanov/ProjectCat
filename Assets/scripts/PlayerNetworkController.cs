c886f458ea3913529d5765ba026393399a48039f
Iordanov <iordano2@illinois.edu>
2017-04-20 15:50:21 -0500

    initial commit with Photon tutorial

---
+++ Assets/scripts/PlayerNetworkController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNetworkController : Photon.MonoBehaviour
+{
+	public GameObject avatar;
+
+	public Transform playerGlobal;
+	public Transform playerLocal;
+
+	void Start ()
+	{
+		Debug.Log("i'm instantiated");
+
+		if (photonView.isMine)
+		{
+			Debug.Log("player is mine");
+
+			playerGlobal = GameObject.Find("OVRPlayerController").transform;
+			if (playerGlobal == null) {
+				Debug.Log("cannot find playerGlobal");
+			}
+			playerLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+
+			this.transform.SetParent(playerLocal);
+			this.transform.localPosition = Vector3.zero;
+
+			// avatar.SetActive(false);
+		}
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			stream.SendNext(playerGlobal.position);
+			stream.SendNext(playerGlobal.rotation);
+			stream.SendNext(playerLocal.localPosition);
+			stream.SendNext(playerLocal.localRotation);
+		}
+		else
+		{
+			this.transform.position = (Vector3)stream.ReceiveNext();
+			this.transform.rotation = (Quaternion)stream.ReceiveNext();
+			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
