391de24dc53055bf9ee129973f0f13b69dd33974
Iordanov <iordano2@illinois.edu>
2017-04-25 20:23:27 -0500

    set up of touch controllers WIP

--- Assets/scripts/PlayerNetworkController.cs
+++ Assets/scripts/PlayerNetworkController.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class PlayerNetworkController : Photon.MonoBehaviour
 {
 	public GameObject avatar;
+	public GameObject lHand;
+	public GameObject rHand;
 
 	public Transform playerGlobal;
 	public Transform playerLocal;
+
+	public Transform lHandLocal;
+	public Transform rHandLocal;
 
 	void Start ()
 	{
@@ -20,13 +25,31 @@ public class PlayerNetworkController : Photon.MonoBehaviour
 			if (playerGlobal == null) {
 				Debug.Log("cannot find playerGlobal");
 			}
-			playerLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+			playerLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor").transform;
+			lHandLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/LeftHandAnchor").transform;
+			rHandLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/RightHandAnchor").transform;
+			if (lHandLocal == null) {
+				Debug.Log ("lHandLocal not instantiated");
+			}
+			if (rHandLocal == null) {
+				Debug.Log ("rHandLocal not instantiated");
+			}
 
 			this.transform.SetParent(playerLocal);
+
 			this.transform.localPosition = Vector3.zero;
 
-			// avatar.SetActive(false);
 		}
+	}
+
+	void LateUpdate() {
+		//avatar.transform.localPosition = playerLocal.position;
+		//avatar.transform.localRotation = playerLocal.rotation;
+
+		lHand.transform.position = lHandLocal.position;
+		lHand.transform.rotation = lHandLocal.rotation;
+		rHand.transform.position = rHandLocal.position;
+		rHand.transform.localRotation = rHandLocal.rotation;
 	}
 
 	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -37,6 +60,11 @@ public class PlayerNetworkController : Photon.MonoBehaviour
 			stream.SendNext(playerGlobal.rotation);
 			stream.SendNext(playerLocal.localPosition);
 			stream.SendNext(playerLocal.localRotation);
+
+			stream.SendNext(lHandLocal.position);
+			stream.SendNext(lHandLocal.rotation);
+			stream.SendNext(rHandLocal.localPosition);
+			stream.SendNext(rHandLocal.localRotation);
 		}
 		else
 		{
@@ -44,6 +72,11 @@ public class PlayerNetworkController : Photon.MonoBehaviour
 			this.transform.rotation = (Quaternion)stream.ReceiveNext();
 			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
 			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+
+			lHand.transform.localPosition = (Vector3)stream.ReceiveNext();
+			lHand.transform.localRotation = (Quaternion)stream.ReceiveNext();
+			rHand.transform.localPosition = (Vector3)stream.ReceiveNext();
+			rHand.transform.localRotation = (Quaternion)stream.ReceiveNext();
 		}
 	}
 }
