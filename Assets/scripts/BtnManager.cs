cb087418867170278799785f3e4a032ad89fa65e
Iordanov <iordano2@illinois.edu>
2017-04-28 19:20:57 -0500

    mainmenu without blend files

---
+++ Assets/scripts/BtnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BtnManager : MonoBehaviour {
+
+
+    public void NewGameBtn()
+    {
+        Debug.Log("New Game Starting");
+        SceneManager.LoadScene("mainscene");
+    }
+
+    public void ControlsBtn()
+    {
+        Debug.Log("Going to Controls");
+        SceneManager.LoadScene("controlscene");
+    }
+
+    public void ExitBtn()
+    {
+        Debug.Log("Quitting Application");
+        Application.Quit();
+    }
+}
---
+++ Assets/scripts/ChangeBtnSize.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeBtnSize : MonoBehaviour {
+    void OnMouseOver()
+    {
+        //Debug.Log("Meehika is smart");
+        this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(180,40);//rect.Set(-93, -2.41f, 160, 30);
+    }
+    void OnMouseExit()
+    {
+        this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 30);
+    }
+}
